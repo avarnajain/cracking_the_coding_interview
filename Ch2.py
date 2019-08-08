b5bf10ac360b318fb612204172ffb7c2def1d7b1
Avarna Jain <avarnajain@gmail.com>
2019-08-08 01:38:43 +0000

    2.2 and 2.3 have doubts, 2.4 done

--- Ch2.py
+++ Ch2.py
@@ -43,7 +43,7 @@ class LinkedList(object):
         current = self.head
         while current:
             if current.data == data:
-                return True
+                return current
             current = current.next
         return False
 
@@ -160,4 +160,114 @@ class DoublyLinkedList(object):
     #         self.tail = h
     #     return
 
+#basic ll test case
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+
 print("----------2.1----------")
+def rem_dups(ll):
+    current = ll.head
+    l = set()
+    while current:
+        if current.data in l:
+            ll.remove(current.data)
+        l.add(current.data)
+        current = current.next
+    return ll
+def rem_dups_runner(ll):
+    current = ll.head
+    while current:
+        runner = current.next
+        while runner:
+            if current.data == runner.data:
+                ll.remove(runner.data)
+            runner = runner.next
+        current = current.next
+    return ll
+# ll.append(2)
+# print(rem_dups(ll))
+# ll.append(2)
+# print(rem_dups_runner(ll))
+
+print("----------2.2----------")
+def kth_to_last_element(ll, k):
+    if not ll.head:
+        return 0
+    index = ll.length() - k + 1
+    count = 1
+    current = ll.head
+    while current:
+        if count==index:
+            return count
+        count += 1
+        current = current.next
+    return 0
+def kth_to_last_recursive(ll, k):
+    if not ll.head:
+        return 0
+    index = ll.length() - k + 1
+    print('index', index)
+    kth = iterate_to_k(ll.head, index, i=1)
+    return kth
+def iterate_to_k(node, index, i):
+    print('node', node, 'index', index, 'i', i)
+    if i == index:
+        return node
+    if i > index or not node:
+        return 0
+    iterate_to_k(node.next, index, i+1)
+# ll.append(6)
+# print(kth_to_last_element(ll, 3))
+# print(kth_to_last_element(ll, 1))
+# print(kth_to_last_element(ll, 7))
+# print(kth_to_last_recursive(ll, 3))
+# print(kth_to_last_recursive(ll, 1))
+# print(kth_to_last_recursive(ll, 7))
+
+print("----------2.3----------")
+def delete_input_node(node):
+    if not node or not node.next:
+        return False
+    _next = node.next
+    node.data = _next.data
+    node.next = _next.next
+    return True
+# print(delete_input_node(ll.find(3)))
+
+print("----------2.4----------")
+def partition(ll, x):
+    if not ll.head:
+        return False
+    lower = LinkedList()
+    higher = LinkedList()
+    current = ll.head
+    while current:
+        if current.data < x:
+            lower.append(current.data)
+        else:
+            higher.append(current.data)
+        current = current.next
+    current = higher.head
+    while current:
+        lower.append(current.data)
+        current = current.next
+    return lower
+
+ll = LinkedList()
+ll.append(1)
+ll.append(5)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(6)
+print(partition(ll, 3))
+print(partition(ll, 4))
+
+
+
+
+
